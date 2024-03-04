6163c35643849f179563ad1c05451e0f2c3c2974
DeLL <elkohenmed1@gmail.com>
2024-03-04 16:21:32 +0100

    main

--- ex 1/cmake-build-debug/cmake_install.cmake
+++ ex 1/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/DeLL/CLionProjects/Ex1
+# Install script for directory: C:/Users/DeLL/OneDrive/Bureau/language C/ex 1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/DeLL/CLionProjects/Ex1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/DeLL/OneDrive/Bureau/language C/ex 1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
